7af9c57d4b0e4912fbc2f242dfe0d6098594a502
Anuj Yadav <anujyadav@Anujs-MacBook-Pro.local>
2024-08-03 20:23:30 +0530

    added netligy

--- app/hooks/todo.js
+++ app/hooks/todo.js
@@ -14,7 +14,7 @@ let dummyTodos = [
     {
         account:{
             idx: '0',
-            content: 'Finish the essay collaboration',
+            content: 'the essay collaboration',
             marked: false,
         }
 
